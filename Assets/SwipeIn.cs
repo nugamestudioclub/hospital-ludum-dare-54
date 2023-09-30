5952eecdf755063b0ee21ab7a6e41ca71d210702
Austin Szema <aszema13@gmail.com>
2023-09-30 14:24:13 -0400

    Improved clipboard functionality

---
+++ Assets/SwipeIn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SwipeIn : MonoBehaviour
+{
+
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform _endPoint;
+
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        transform.position = _startPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _endPoint.position, 1f);
+    }
+}
