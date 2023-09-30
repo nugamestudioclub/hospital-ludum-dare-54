3acbeac749ab3c79dedb24b3f9536d216f0e6a20
Pakmanlie <jianwen_ding@icloud.com>
2023-09-30 12:52:45 -0400

    pesfd

--- Assets/Scripts/RoomSystem.cs
+++ Assets/Scripts/RoomSystem.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class RoomSystem : MonoBehaviour
 {
+
+    [SerializeField]
+    private static float severityToTimeRatio = 20;
+    [SerializeField]
+    private static float severityToFailRatio = 7;
+    public class Patient 
+    {
+        public string firstName;
+        public string lastName;
+        //Time it takes for patient to leave room
+        public float fixTime;
+        //Out of 100%
+        public float failRatio;
+        public int insuranceValue;
+        Patient(string getFirst, string getLast, int getSeverity, int getInsuranceValue)
+        {
+            firstName = getFirst;
+            lastName = getLast;
+            insuranceValue = getInsuranceValue;
+            fixTime = getSeverity * severityToTimeRatio;
+            failRatio = getSeverity * severityToFailRatio;
+        }
+    }
+
+    List<Patient> waitList;
+    
     // Start is called before the first frame update
     void Start()
     {
